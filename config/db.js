8ec691ed015e929d6ba00151f1f8ee4272419f31
David Whipple <dwhipp88@gmail.com>
2023-01-16 21:33:14 -0700

    adjusted config/db.js for deployment

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI');
+// const db = config.get('mongoURI');
+const db = process.env.DATABASE_URL;
 
 // mongoose.connect(db);
 const connectDB = async () => {
