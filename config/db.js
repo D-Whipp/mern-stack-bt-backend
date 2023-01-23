f80f2c5f66bf1eec4c0dce05b547d4798dcc9434
David Whipple <dwhipp88@gmail.com>
2023-01-23 14:43:15 -0700

    running database checks

--- config/db.js
+++ config/db.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 const config = require('config');
 // const db = config.get('mongoURI');
-const db = process.env.DATABASE_URL;
-// const uname = process.env.uname;
-// const dbpassword = process.env.dbpassword;
-// const db =
-//   'mongodb+srv://' +
-//   uname +
-//   ':' +
-//   dbpassword +
-//   '@devconnector.unlftvb.mongodb.net/?retryWrites=true&w=majority';
+// const db = process.env.DATABASE_URL;
+const uname = process.env.uname;
+const dbpassword = process.env.dbpassword;
+const db =
+  'mongodb+srv://' +
+  uname +
+  ':' +
+  dbpassword +
+  '@devconnector.unlftvb.mongodb.net/?retryWrites=true&w=majority';
 
-// mongoose.connect(db);
+mongoose.connect(db);
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
